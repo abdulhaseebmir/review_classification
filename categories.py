279cc0d6536bfa20ac8ce315e53b6eddeaeda417
stein <mabdulhaseebq@gmail.com>
2023-09-11 20:14:43 +0500

    pre-defined summarized categories

---
+++ categories.py
@@ -0,0 +1,8 @@
+category_summaries = {
+    "Food Quality": "This review center on criticisms of the flavor and seasoning of the food, pointing out instances where the taste doesn't meet expectations or lacks balance.",
+    "Customer Service": "This review revolve around critiques of the restaurant's service quality, highlighting instances where the service is inattentive, discourteous, or fails to meet the expectations of diners.",
+    "Sanitary Concerns": "This review focus on concerns related to the cleanliness and hygiene of the restaurant, often pointing out issues like dirty tables and floors or instances where cleanliness standards are not met",
+    "Long Wait": "This review highlights concerns related to extended waiting times and reservation issues at the restaurant, specifically noting instances where customers had to endure lengthy delays despite making reservations.",
+    "Value for Money": "This review centers on the perceived value for money at the restaurant, underscoring instances where patrons felt the portion sizes were inadequate or the prices were excessively high, leaving them dissatisfied with the overall value of their dining experience.",
+    "Allergy": "This review highlights concerns related to allergies or dietary restrictions, particularly when the restaurant fails to accommodate these needs, potentially posing health risks to patrons."
+}
